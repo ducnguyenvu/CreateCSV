02a38b87832a422d8cd1094e9b665eba32d918a3
duc20062001@gmail.com <19521381@gm.uit.edu.vn>
2023-03-22 23:44:27 +0700

    Update reload new data in csv in google drive
    Update reload new data in csv in google drive

--- CreateCSV/CreateCSV/Program.cs
+++ CreateCSV/CreateCSV/Program.cs
@@ -1,19 +1,12 @@
 ﻿using CreateCSV.Model;
-using CsvHelper;
-using CsvHelper.Configuration;
 using Google.Apis.Auth.OAuth2;
 using Google.Apis.Drive.v3;
 using Google.Apis.Services;
-using Google.Apis.Upload;
-using System;
 using System.Collections.Generic;
 using System.Configuration;
-using System.Data;
 using System.Data.SqlClient;
 using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices;
-using System.Security.Policy;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -22,11 +15,13 @@ namespace CreateCSV
 {
     internal class Program
     {
-        private const string PathToServiceAccountKeyFile = @"D:\Spiraledge\Create_CSV_Final\Create_CSV_New\CreateCSV\CreateCSV\bin\Debug\testcreatecsv-c96d103fe5c1.json";
+        private const string PathToServiceAccountKeyFile = "testcreatecsv-c96d103fe5c1.json";
         private const string ServiceAccountEmail = "testcreatecsv1@testcreatecsv.iam.gserviceaccount.com";
-        private const string UploadFileName = @"D:\Spiraledge\Create_CSV_Final\Create_CSV_New\CreateCSV\CreateCSV\products.csv";
+        private const string UploadFileProductEY = "ListProductEY.csv";
+        private const string UploadFileProductSO = "ListProductSO.csv";
+        private const string path = @"E:\Spiraledge\CreateCSV_New\CreateCSV\CreateCSV\CreateCSV\bin\Debug";
         private const string DirectoryId = "1BXAwnZQJz3V3vartHdY9562aNPxUuVUD";
-        public static void Export(List<Product> products, string site)
+        public static async Task Export(List<Product> products, string site)
         {
             StringBuilder stringBuilder = new StringBuilder();
             stringBuilder.AppendLine("Collection_ID,Collection_Name");
@@ -35,9 +30,9 @@ namespace CreateCSV
                 stringBuilder.AppendLine($"{prod.CollectionID},{prod.CollectionName}");
             }
             // export file csv to folder
-            if(site == "EY")
+            if (site == "EY")
             {
-              File.WriteAllText("ListProductEY.csv",stringBuilder.ToString());
+                File.WriteAllText("ListProductEY.csv", stringBuilder.ToString());
             }
             else
             {
@@ -47,9 +42,10 @@ namespace CreateCSV
         public static List<Product> GetAllProductsSO()
         {
             var connectionString = ConfigurationManager.ConnectionStrings["Connection21"].ConnectionString;
-            var query = "SELECT m.ShopifyId[CollectionID], c.CategoryName[CollectionName] " +
-                "from beta_swimoutlet_com_standalone.dbo.SOT_SHOPIFY_IDS_MAPPING_SWIMOUTLET_COM m with(nolock)" +
-                "join Categories c with(nolock) on m.OurId = c.CategoryID where m.TableName='categories' and m.OurSite='Swimoutlet' ORDER by c.CategoryID";
+            //var query = "SELECT m.ShopifyId[CollectionID], c.CategoryName[CollectionName] " +
+            //    "from beta_swimoutlet_com_standalone.dbo.SOT_SHOPIFY_IDS_MAPPING_SWIMOUTLET_COM m with(nolock)" +
+            //    "join Categories c with(nolock) on m.OurId = c.CategoryID where m.TableName='categories' and m.OurSite='Swimoutlet' ORDER by c.CategoryID";
+            var query = "select ProductId[CollectionID], NameProduct[CollectionName] from [dbo].[Product]";
             List<Product> products = new List<Product>();
 
             using (SqlConnection connection = new SqlConnection(connectionString))
@@ -98,7 +94,7 @@ namespace CreateCSV
             }
             return products;
         }
-        public static async Task Upload()
+        public static async Task Upload(string uploadFileProduct)
         {
             // Load the Service account credentials and define the scope of its access.
             var credential = GoogleCredential.FromFile(PathToServiceAccountKeyFile)
@@ -111,74 +107,46 @@ namespace CreateCSV
             // Upload file Metadata
             var fileMetadata = new Google.Apis.Drive.v3.Data.File()
             {
-                Name = UploadFileName,
+                Name = uploadFileProduct,
                 Parents = new List<string>() { DirectoryId }
             };
-            // find the file we uploaded by name 
-            using (var fsSource = new FileStream(UploadFileName, FileMode.Open, FileAccess.Read))
+
+            FilesResource.ListRequest listRequest = service.Files.List();
+            IList<Google.Apis.Drive.v3.Data.File> files = listRequest.Execute().Files;
+            var uploadedFile = files.FirstOrDefault(f => f.Name == uploadFileProduct);
+            if (uploadedFile == null)
             {
-                // Create a new file, with metadata and stream.
-                var request = service.Files.Create(fileMetadata, fsSource, "csv/plain");
-                request.Fields = "*";
-                var results = await request.UploadAsync(CancellationToken.None);
+                // find the file we uploaded by name 
+                using (var fsSource = new FileStream(uploadFileProduct, FileMode.Open, FileAccess.Read))
+                {
+                    // Create a new file, with metadata and stream.
+                    var request = service.Files.Create(fileMetadata, fsSource, "text/csv");
+                    request.Fields = "*";
+                    var results = await request.UploadAsync(CancellationToken.None);
+                }
             }
-
-            //var files = await service.Files.List().ExecuteAsync(CancellationToken.None);
-            //var uploadedFile = files.Files.FirstOrDefault(f => f.Name == UploadFileName);
-            //if (uploadedFile == null)
-            //{
-            //    //Create a new file on Google Drive
-                
-            //}
-            //else
-            //{
-            //    //// Note: not all fields are writeable watch out, you cant just send uploadedFile back.
-            //    var updateFileBody = new Google.Apis.Drive.v3.Data.File()
-            //    {
-            //        Name = "Test hello 123.txt",
-            //    };
-
-            //    //// Let's add some text to our file.
-            //    var updateFileContent = "Hello World 123 123!";
-            //    System.IO.File.WriteAllText(UploadFileName, updateFileContent);
-
-
-            //    //// Then upload the file again with a new name and new data.
-            //    using (var uploadStream = new FileStream(UploadFileName, FileMode.Open, FileAccess.Read))
-            //    {
-            //        // Update the file id, with new metadata and stream, don't update file name.
-
-            //        var updateRequest = service.Files.Update(updateFileBody, uploadedFile.Id, uploadStream, "text/plain");
-            //        var result = updateRequest.UploadAsync(CancellationToken.None);
-            //    }
-            //}
+            else
+            {
+                //// Note: not all fields are writeable watch out, you cant just send uploadedFile back.
+                var updateFileBody = new Google.Apis.Drive.v3.Data.File()
+                {
+                    Name = uploadFileProduct,
+                };
+                //// Then upload the file again with a new name and new data.
+                using (var uploadStream = new FileStream(uploadFileProduct, FileMode.Open, FileAccess.Read))
+                {
+                    // Update the file id, with new metadata and stream.
+                    var updateRequest = service.Files.Update(updateFileBody, uploadedFile.Id, uploadStream, "text/csv");
+                    var result = await updateRequest.UploadAsync(CancellationToken.None);
+                }
+            }
         }
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            //Export(GetAllProductsSO(),"SO");
-            //Export(GetAllProductsEY(),"EY");
-            //Upload();
-            var credential = GoogleCredential.FromFile(PathToServiceAccountKeyFile)
-                            .CreateScoped(DriveService.ScopeConstants.Drive);
-            // Create the  Drive service.
-            var service = new DriveService(new BaseClientService.Initializer()
-            {
-                HttpClientInitializer = credential
-            });
-            // Upload file Metadata
-            var fileMetadata = new Google.Apis.Drive.v3.Data.File()
-            {
-                Name = UploadFileName,
-                Parents = new List<string>() { DirectoryId }
-            };
-            // find the file we uploaded by name 
-             using (var fsSource = new FileStream(UploadFileName, FileMode.Open, FileAccess.Read))
-            {
-                // Create a new file, with metadata and stream.
-                var request = service.Files.Create(fileMetadata, fsSource, "csv/plain");
-                request.Fields = "*";
-                var results =  request.UploadAsync(CancellationToken.None);
-            }
+            await Export(GetAllProductsSO(), "SO");
+            //await Export(GetAllProductsEY(),"EY");
+            //await Upload(UploadFileProductEY);
+            await Upload(UploadFileProductSO);
         }
 
     }
